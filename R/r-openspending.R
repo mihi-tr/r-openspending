3b73470909bda25d569cc6565e40274edae69767
Michael Bauer <mihi@lo-res.org>
2013-08-14 19:58:40 +0200

    stupid me paste collapse does the job

--- R/r-openspending.R
+++ R/r-openspending.R
@@ -16,30 +16,13 @@ openspending.datasets <- function(territory=NA,language=NA) {
   return(data$datasets)
   }
 
-openspending.preparevector <- function(v, r=NA) {
-  if (length(v)==1) {
-    if (is.na(r)) {
-      return(v[1])
-      }
-    else {
-      return(paste(r,v[1],sep="|"))
-      }
-      }
-  if (is.na(r)) {
-    return(openspending.preparevector(v[seq(2,length(v))],v[1]))
-    }
-  else {  
-    return(
-    openspending.preparevector(v[seq(2,length(v))],paste(r,v[1],sep="|")))
-  }
-  }
 openspending.aggregate <- function(dataset, cut=NA, drilldown=NA, measure="amount") {
   url=paste(openspending.api,"aggregate?dataset=",dataset,"&measure=",measure,sep="")
   if (!is.na(cut)) {
     url=paste(url,"&cut=",cut,sep="")
     }
-  if (!is.na(drilldown)) {
-    url=paste(url,"&drilldown=",preparevector(drilldown),sep="")
+  if (!is.na(drilldown[1])) {
+    url=paste(url,"&drilldown=",paste(drilldown,collapse="|"),sep="")
     };
   j=getURL(url)
   data=fromJSON(j)
